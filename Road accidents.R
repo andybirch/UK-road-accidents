f01372def1c06111b880da8a8cbe50427f188a0d
Andy Birch <andybirch@andys-mbp.home>
2021-06-25 12:15:58 +0100

    Update input data

--- Road accidents.R
+++ Road accidents.R
@@ -1,3 +1,16 @@
 library(tidyverse)
 library(readr)
 library(lubridate)
+library(RCurl)
+
+
+x <- getURL("https://raw.githubusercontent.com/andybirch/UK-road-accidents/master/accidents.csv")
+accidents <- read.csv(text = x)
+x <- getURL("https://raw.githubusercontent.com/andybirch/UK-road-accidents/master/casualties.csv")
+casualties <- read.csv(text = x)
+x <- getURL("https://raw.githubusercontent.com/andybirch/UK-road-accidents/master/vehicles.csv")
+vehicles <- read.csv(text = x)
+
+view(accidents)
+view(casualties)
+view(vehicles)
